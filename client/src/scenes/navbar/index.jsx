315284ee841f81b7d83ea83eef4718a1e06c4995
megatron3002 <omkar.300422@gmail.com>
2024-04-17 11:57:18 +0530

    register, login page

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
         <FlexBetween gap="1.75 rem">
             <Typography 
                 fontWeight="bold" 
-                fontSize="clamp(1rem,2rem,2.25rem" 
+                fontSize="clamp(1rem,2rem,2.25rem)" 
                 color="primary" 
                 onClick={() => navigate("/home")} 
                 sx={{
